c1e3e9c852a325ad227500d11411ccbbe121ca96
Prakhar Dhyani <prakhardhyani1000@gmail.com>
2022-02-05 21:52:05 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Navbar from "./Components/Navbar"
 
 function App() {
   return (
-    <div >
-     <h1>HEllo</h1>
-    </div>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div style={{display:"flex", background:"lightblue" }}>
+                <h1 style={{paddingTop:"1rem", paddingLeft:"1rem"}}>Movies App</h1>
+                <h3 style={{paddingTop:"1.5rem", paddingLeft:"1.5rem"}}>favourites</h3>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
